7ab1c1c46b6e6d810fa49232e0face9d436c9886
lgrimm6 <robertofernandesfreitas@gmail.com>
2022-11-03 20:00:14 -0300

    Feat: list all users router get

--- src/controllers/users/listUsers.controller.ts
+++ src/controllers/users/listUsers.controller.ts
@@ -1,3 +1,11 @@
-const listUsersController = async () => {};
+import { Request, Response } from 'express';
+import listUsersServices from '../../services/users/listUser.services';
+import { instanceToPlain } from 'class-transformer';
+import { IUser } from '../../interface/users/users';
+
+const listUsersController = async (req: Request, res: Response) => {
+  const users: IUser[] = await listUsersServices();
+  return res.status(201).json(instanceToPlain(users));
+};
 
 export default listUsersController;
