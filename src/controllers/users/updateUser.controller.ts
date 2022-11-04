04298fa3a678de54b112af6f1c8eb18674d15b41
lgrimm6 <robertofernandesfreitas@gmail.com>
2022-11-04 18:17:48 -0300

    Feat: router update user users/update/:id

--- src/controllers/users/updateUser.controller.ts
+++ src/controllers/users/updateUser.controller.ts
@@ -1,3 +1,19 @@
-const updateUserController = async () => {};
+import { Request, Response } from 'express';
+import { instanceToPlain } from 'class-transformer';
+import { User } from '../../entities/user.entity';
+import { IUserUpdate } from '../../interface/users/users';
+import updatedUserService from '../../services/users/updateUser.services';
+import { AppError, handleError } from '../../errors/appError';
+
+const updateUserController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const id: string = req.params.id;
+  const update: IUserUpdate = req.body;
+  const updatedUser: User | null = await updatedUserService(id, update);
+
+  return res.json(instanceToPlain(updatedUser));
+};
 
 export default updateUserController;
