2b0757394cd3caf2d48920557e3d8b17e456bc9c
lgrimm6 <robertofernandesfreitas@gmail.com>
2022-11-04 18:16:43 -0300

    Feat: router list user /users/:id

--- src/router/users/users.routes.ts
+++ src/router/users/users.routes.ts
@@ -3,10 +3,10 @@ import { Router } from 'express';
 import createUserController from '../../controllers/users/createUser.controller';
 import softDeleteUserController from '../../controllers/users/softDeleteUser.controller';
 import deleteUserController from '../../controllers/users/deleteUser.controller';
-/* import updateUserController from '../../controllers/users/updateUser.controller';
+import updateUserController from '../../controllers/users/updateUser.controller';
 
- */
 import listUsersController from '../../controllers/users/listUsers.controller';
+import listUserController from '../../controllers/users/listUser.controller';
 
 /* import verifyAuthTokenMiddleware from '../../middlewares/verifyAuthToken.middlewares'; */
 /* import verifyUpdateData from '../../middlewares/verifyUpdateData'; */
@@ -17,13 +17,13 @@ router.post('/create', createUserController);
 router.delete('/desactive/:id', softDeleteUserController);
 router.delete('/delete/:id', deleteUserController);
 router.get('', /* verifyAuthTokenMiddleware,  */ listUsersController);
+router.get('/:id', /* verifyAuthTokenMiddleware,  */ listUserController);
 
-/* router.patch(
+router.patch(
   '/update/:id',
-  verifyUpdateData,
-  verifyAuthTokenMiddleware,
+  /*   verifyUpdateData,
+  verifyAuthTokenMiddleware, */
   updateUserController
 );
-*/
 
 export default router;
