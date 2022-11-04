56172a2c7fef985c533ccfa60a7d6560781ee4ce
richzago <rich.czago@gmail.com>
2022-11-04 15:16:50 -0300

    feat: Added fields route

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,14 @@ import express, { Request, Response, NextFunction } from 'express';
 import usersRoutes from './router/users/users.routes';
 import { AppError } from './errors/appError';
 import userLoginRouter from './router/login/userLogin.routes';
+import fieldRoutes from './router/fields/fields.routes';
 
 const app = express();
 app.use(express.json());
 
 app.use('/users', usersRoutes);
 app.use('/users/login', userLoginRouter);
+app.use('/fields', fieldRoutes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
