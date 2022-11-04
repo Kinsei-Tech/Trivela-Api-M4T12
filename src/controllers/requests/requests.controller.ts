de46f79d901cc9313adc57ef98898a8c9360f988
lgrimm6 <robertofernandesfreitas@gmail.com>
2022-11-04 15:25:06 -0300

    Fix: request controllers

--- src/controllers/requests/requests.controller.ts
+++ src/controllers/requests/requests.controller.ts
@@ -1,17 +1,17 @@
-import { Request, Response } from "express";
-import requestsCreateService from "../../services/requests/requestsCreate.service";
-import requestsListService from "../../services/requests/requestsList.service";
+import { Request, Response } from 'express';
+import requestsCreateService from '../../services/requests/requestsCreate.service';
+import requestsListService from '../../services/requests/requestsList.service';
 
 const requestsListController = async (req: Request, res: Response) => {
-    const data = req.body;
-    const createdRequestsInfo = await requestsCreateService(data);
-  
-    return res.status(201).json(createdRequestsInfo);
-}
-
-const  requestsCreateController = async (req: Request, res: Response) => {
-    const requests = await requestsListService();
+  const id: string = req.body.id;
+  const requests = await requestsListService(id);
   return res.json(requests);
-}
+};
+
+const requestsCreateController = async (req: Request, res: Response) => {
+  const data = req.body;
+  const createdRequestsInfo = await requestsCreateService(data);
+  return res.status(201).json(createdRequestsInfo);
+};
 
 export { requestsListController, requestsCreateController };
