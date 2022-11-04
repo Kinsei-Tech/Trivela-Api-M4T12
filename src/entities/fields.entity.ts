c3ab666a544081d1d8bdd0282488993715760143
richzago <rich.czago@gmail.com>
2022-11-04 15:17:58 -0300

    feat: added new isActive column

--- src/entities/fields.entity.ts
+++ src/entities/fields.entity.ts
@@ -1,27 +1,35 @@
-import { Column, Entity, JoinColumn, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
-import { Adress } from "./adress.entity";
-import { Owner } from "./owner.entity"
-import { Team } from "./team.entity";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Adress } from './adress.entity';
+import { Owner } from './owner.entity';
+import { Team } from './team.entity';
 
-@Entity('fields') 
-export class Field{
-    @PrimaryGeneratedColumn('uuid')
-    id: string
+@Entity('fields')
+export class Field {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column()
-    name: string
+  @Column()
+  name: string;
 
+  @Column({ default: true })
+  isActive: boolean;
 
-    @ManyToOne(() => Owner, (owner) => owner, {eager: true})
-    @JoinColumn()
-    owners: Owner
+  @ManyToOne(() => Owner, (owner) => owner, { eager: true })
+  @JoinColumn()
+  owners: Owner;
 
+  @OneToOne(() => Adress, { eager: true })
+  @JoinColumn()
+  adresses: Adress;
 
-    @OneToOne(() => Adress, {eager: true})
-    @JoinColumn()
-    adresses: Adress
-
-    @OneToMany(() => Team, (team) => team.fields)
-    team: Team[]
-
+  @OneToMany(() => Team, (team) => team.fields)
+  team: Team[];
 }
