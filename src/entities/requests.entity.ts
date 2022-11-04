1ed81e35b158e1d394fcaa2aadf36411a298b856
lgrimm6 <robertofernandesfreitas@gmail.com>
2022-11-04 15:22:43 -0300

    Fix : Entity request default status

--- src/entities/requests.entity.ts
+++ src/entities/requests.entity.ts
@@ -1,23 +1,30 @@
-import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import { Team } from "./team.entity";
-import { User } from "./user.entity";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Team } from './team.entity';
+import { User } from './user.entity';
 
 @Entity('requests')
-export class Request{
-    @PrimaryGeneratedColumn('uuid')
-    id: string
+export class Request {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column()
-    status: number
- 
-    @Column()
-    positions: number
+  @Column({ default: 1 })
+  status: number;
 
-    @ManyToOne(() => User, (user) => user.request)
-    @JoinColumn()
-    users: User
+  @Column()
+  positions: number;
 
-    @ManyToOne(() => Team, (team) => team.request)
-    @JoinColumn()
-    teams: Team
+  @ManyToOne(() => User, (user) => user.request)
+  @JoinColumn()
+  users: User;
+
+  @ManyToOne(() => Team, (team) => team.request)
+  @JoinColumn()
+  teams: Team;
 }
