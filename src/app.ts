c744e5a7fbfb699b2ffa49fbe8f628fbf293d90d
Laís Martins <silveiramartinslais@gmail.com>
2022-11-07 05:05:36 -0300

    feat: teams routes configured in express

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'express-async-errors';
 import express, { Request, Response, NextFunction } from 'express';
 import usersRoutes from './router/users/users.routes';
+import teamsRoutes from './router/teams/teams.routes';
 import { AppError } from './errors/appError';
 import userLoginRouter from './router/login/userLogin.routes';
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/users', usersRoutes);
+app.use('/teams', teamsRoutes);
 app.use('/users/login', userLoginRouter);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
