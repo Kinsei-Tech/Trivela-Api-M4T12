0e1cee083931538bb730964bc564fb9f2f2d13c5
Laís Martins <silveiramartinslais@gmail.com>
2022-11-07 05:15:37 -0300

    feat: createdAt, updatedAt and isActive added

--- src/entities/owner.entity.ts
+++ src/entities/owner.entity.ts
@@ -1,16 +1,32 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('owners')
 export class Owner {
-    @PrimaryGeneratedColumn('uuid')
-    id: string 
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column()
-    name: string
+  @Column()
+  name: string;
 
-    @Column()
-    email: string
-    
-    @Column()
-    password: string
+  @Column()
+  email: string;
+
+  @Column('boolean', { default: true, nullable: true })
+  isActive: boolean = true;
+
+  @CreateDateColumn({ nullable: true })
+  createdAt: Date;
+
+  @UpdateDateColumn({ nullable: true })
+  updatedAt: Date;
+
+  @Column()
+  password: string;
 }
