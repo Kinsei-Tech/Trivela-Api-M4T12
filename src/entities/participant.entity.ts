0a12995606e77c4cc6a5af17babe3e6cf6d43a63
Laís Martins <silveiramartinslais@gmail.com>
2022-11-07 05:17:02 -0300

    fix: Teams and Participants have a ManyToMany relation. feat: createdAt, updatedAt and isActive added

--- src/entities/participant.entity.ts
+++ src/entities/participant.entity.ts
@@ -1,21 +1,30 @@
-import { Column, Entity, JoinColumn, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
-import { Team } from "./team.entity";
-import { User } from "./user.entity";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Team } from './team.entity';
+import { User } from './user.entity';
 
 @Entity('participants')
-export class Participant{ 
-    @PrimaryGeneratedColumn('uuid')
-    id: string
+export class Participant {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column()
-    position: number
+  @Column()
+  position: number;
 
-    @ManyToOne(() => Team, (team) => team.id, {eager: true})
-    @JoinColumn()
-    teams: Team
+  @ManyToMany(() => Team, (team) => team.id, { eager: true })
+  @JoinTable()
+  team: Team;
 
-    @OneToOne(() => User, {eager: true})
-    @JoinColumn()
-    users: User
-
+  @OneToOne(() => User, { eager: true })
+  @JoinColumn()
+  user: User;
 }
