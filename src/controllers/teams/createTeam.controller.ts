f1ea9fc1bb57223df063593f16e346062cbc32d0
Laís Martins <silveiramartinslais@gmail.com>
2022-11-07 16:01:22 -0300

    fix: create team endpoint

--- src/controllers/teams/createTeam.controller.ts
+++ src/controllers/teams/createTeam.controller.ts
@@ -4,8 +4,9 @@ import createTeamService from '../../services/teams/createTeam.service';
 
 const createTeamController = async (req: Request, res: Response) => {
   const teamData: ITeamRequest = req.body;
-  const userId: string = req.user.id;
+  const userId = req.user.id;
   const team = await createTeamService(teamData, userId);
+
   return res.status(201).json({
     message: 'Time criado com sucesso!',
     team: team,
