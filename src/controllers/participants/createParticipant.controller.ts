fbcada0defdab038539fc22d9d4469aac60e50a7
Laís Martins <silveiramartinslais@gmail.com>
2022-11-07 16:36:18 -0300

    feat: create participants controller created

---
+++ src/controllers/participants/createParticipant.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import { IParticipantRequest } from '../../interface/participants/participants';
+import createParticipantService from '../../services/participants/createParticipant.service';
+
+const createParticipantController = async (req: Request, res: Response) => {
+  const data: IParticipantRequest = req.body;
+  const participant = await createParticipantService(data);
+  return res.status(201).json(participant);
+};
+
+export default createParticipantController;
