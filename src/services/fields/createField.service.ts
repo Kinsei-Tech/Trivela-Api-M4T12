f7fc3523372cddb34a99b5a2ab261ff16104e179
richzago <rich.czago@gmail.com>
2022-11-07 17:07:26 -0300

    fix: some bugs in create field

--- src/services/fields/createField.service.ts
+++ src/services/fields/createField.service.ts
@@ -2,18 +2,28 @@ import AppDataSource from '../../data-source';
 import { Adress } from '../../entities/adress.entity';
 import { Field } from '../../entities/fields.entity';
 import { Owner } from '../../entities/owner.entity';
+import { AppError } from '../../errors/appError';
 import { IFieldRequest } from '../../interface/fields/fields';
 
 const createFieldService = async (fieldData: IFieldRequest): Promise<Field> => {
   const fieldsRepository = AppDataSource.getRepository(Field);
-    //  const ownersRepository = AppDataSource.getRepository(Owner);
-  //   const adressesRepository = AppDataSource.getRepository(Adress);
+  const ownersRepository = AppDataSource.getRepository(Owner);
+  const adressesRepository = AppDataSource.getRepository(Adress);
+
+  const getOwner = await ownersRepository.findOneBy({ id: fieldData.ownerId });
+
+  if (!getOwner) {
+    throw new AppError(404, 'Owner not found!');
+  }
+
+  const newAdresses = adressesRepository.create(fieldData.address);
+  await adressesRepository.save(newAdresses);
 
   const newField = new Field();
   newField.name = fieldData.name;
   newField.isActive = fieldData.isActive;
-  // newField.owners = ownersRepository
-  // newField.adresses = adressesRepository
+  newField.owners = getOwner;
+  newField.adresses = newAdresses;
 
   const field: Field = fieldsRepository.create(newField);
   await fieldsRepository.save(field);
