fc8d8a1321a22d88ba53e44a431a6b63b0c6355d
Laís Martins <silveiramartinslais@gmail.com>
2022-11-08 06:26:27 -0300

    fix: http status fixed

--- src/controllers/teams/deleteTeam.controller.ts
+++ src/controllers/teams/deleteTeam.controller.ts
@@ -4,7 +4,7 @@ import deleteTeamService from '../../services/teams/deleteTeam.service';
 const deleteTeamController = async (req: Request, res: Response) => {
   const id = req.params.id;
   await deleteTeamService(id);
-  return res.status(204).json({
+  return res.json({
     message: 'Time deletado com sucesso!',
   });
 };
