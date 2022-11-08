53c8aaa1e1f5235bf94325db755cb3ed5a0ab0a1
Laís Martins <silveiramartinslais@gmail.com>
2022-11-08 20:19:45 -0300

    feat: teams routes middlewares

--- src/router/teams/teams.routes.ts
+++ src/router/teams/teams.routes.ts
@@ -6,6 +6,7 @@ import listTeamsController from '../../controllers/teams/listTeams.controller';
 import listTeamController from '../../controllers/teams/listTeam.controller';
 import updateTeamController from '../../controllers/teams/updateTeam.controller';
 import ensureAuthMiddleware from '../../middlewares/ensureAuth.middleware';
+import isTeamOwnerMiddleware from '../../middlewares/isTeamOwner.middleware';
 
 const router = Router();
 
@@ -13,14 +14,21 @@ router.post('/create', ensureAuthMiddleware, createTeamController);
 router.delete(
   '/deactivate/:id',
   ensureAuthMiddleware,
+  isTeamOwnerMiddleware,
   deactivateTeamController
 );
-router.delete('/delete/:id', ensureAuthMiddleware, deleteTeamController);
+router.delete(
+  '/delete/:id',
+  ensureAuthMiddleware,
+  isTeamOwnerMiddleware,
+  deleteTeamController
+);
 router.get('', ensureAuthMiddleware, listTeamsController);
 router.get('/:id', ensureAuthMiddleware, listTeamController);
 router.patch(
   '/update/:id' /* verifyUpdateData,*/,
   ensureAuthMiddleware,
+  isTeamOwnerMiddleware,
   updateTeamController
 );
 
