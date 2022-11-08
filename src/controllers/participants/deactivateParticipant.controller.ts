cb42de7edd4eb9da0513293476cbbdaec6054861
Laís Martins <silveiramartinslais@gmail.com>
2022-11-08 20:16:49 -0300

    fix: deactivate participant controller fixed

--- src/controllers/participants/deactivateParticipant.controller.ts
+++ src/controllers/participants/deactivateParticipant.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import deactivateParticipantService from '../../services/participants/deactivateParticipant.service';
 
 const deactivateParticipantController = async (req: Request, res: Response) => {
-  const id = req.params.id;
+  const id = req.participant.id;
   await deactivateParticipantService(id);
   return res.json({
     message: 'Participante desativado com sucesso!',
