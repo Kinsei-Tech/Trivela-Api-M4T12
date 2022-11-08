89c4789998a4330ea6ac8d970012d36f2b1189e1
Wigo Rossim da Silva <wigorossim@hotmail.com>
2022-11-08 16:44:17 -0300

    feat: adding environment variable to port

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
-import app from "./app";
-import AppDataSource from "./data-source";
+import app from './app';
+import AppDataSource from './data-source';
+import 'dotenv/config';
+
+
 
 (async () => {
+  await AppDataSource.initialize().catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
 
-    await AppDataSource.initialize()
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err)
-    })
-    
-    app.listen(3000, () => {
-        console.log("Servidor executando")
-    })    
-})() 
+  const PORT = process.env.PORT
+
+  app.listen(PORT, () => {
+    console.log(`Servidor executando na porta ${PORT}`);
+  });
+})();
