3678d71be1fae14692cf4a46679c60d8e5daf256
richzago <rich.czago@gmail.com>
2022-11-07 23:05:17 -0300

    feat: add middlewares in fields routes

--- src/router/fields/fields.routes.ts
+++ src/router/fields/fields.routes.ts
@@ -1,17 +1,33 @@
-import { Router } from "express";
-import createFieldController from "../../controllers/fields/createField.controller";
-import deleteFieldController from "../../controllers/fields/deleteField.controller";
-import listFieldsController from "../../controllers/fields/listFields.controller";
-import softDeleteFieldController from "../../controllers/fields/softDeleteField.controller";
-import updateFieldController from "../../controllers/fields/updateField.controller";
+import { Router } from 'express';
+import createFieldController from '../../controllers/fields/createField.controller';
+import deleteFieldController from '../../controllers/fields/deleteField.controller';
+import listFieldsController from '../../controllers/fields/listFields.controller';
+import softDeleteFieldController from '../../controllers/fields/softDeleteField.controller';
+import updateFieldController from '../../controllers/fields/updateField.controller';
+import ensureAuthMiddleware from '../../middlewares/ensureAuth.middleware';
+import isOwnerField from '../../middlewares/isOwnerField.middleware';
 
-const router = Router()
+const router = Router();
 
-router.post("", createFieldController)
-router.get("/owners/:id", listFieldsController)
-router.patch("/owners/:id", updateFieldController)
-router.delete("/:id", deleteFieldController)
-router.delete("/:id", softDeleteFieldController)
+router.post('', ensureAuthMiddleware, createFieldController);
+router.get('/owners', ensureAuthMiddleware, listFieldsController);
+router.patch(
+  '/owners/:id',
+  ensureAuthMiddleware,
+  isOwnerField,
+  updateFieldController
+);
+router.delete(
+  '/:id',
+  ensureAuthMiddleware,
+  isOwnerField,
+  deleteFieldController
+);
+router.delete(
+  '/:id',
+  ensureAuthMiddleware,
+  isOwnerField,
+  softDeleteFieldController
+);
 
-
-export default router
+export default router;
