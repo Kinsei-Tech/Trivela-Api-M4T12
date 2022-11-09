217ed72fded47ba631e7597f3c4f0157866e7e13
lgrimm6 <robertofernandesfreitas@gmail.com>
2022-11-09 10:13:17 -0300

    fix: fixed migrations

--- src/migrations/1667998769550-create.ts
+++ src/migrations/1667998769550-create.ts
@@ -1,13 +1,13 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class createTables1667907466810 implements MigrationInterface {
-    name = 'createTables1667907466810'
+export class create1667998769550 implements MigrationInterface {
+    name = 'create1667998769550'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "admins" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_e3b38270c97a854c48d2e80874e" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "addresses" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "street" character varying NOT NULL, "district" character varying NOT NULL, "zipCode" character varying NOT NULL, "number" character varying, "complement" character varying, "city" character varying NOT NULL, "state" character varying NOT NULL, CONSTRAINT "PK_745d8f43d3af10ab8247465e450" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "owners" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "createdAt" TIMESTAMP DEFAULT now(), "updatedAt" TIMESTAMP DEFAULT now(), "email" character varying NOT NULL, "isActive" boolean NOT NULL DEFAULT true, "password" character varying NOT NULL, CONSTRAINT "UQ_df4ef717018c5dc7bd3f4ab0de5" UNIQUE ("email"), CONSTRAINT "PK_42838282f2e6b216301a70b02d6" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "fields" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "isActive" boolean DEFAULT true, "createdAt" TIMESTAMP DEFAULT now(), "updatedAt" TIMESTAMP DEFAULT now(), "ownerId" uuid, "addressId" uuid, CONSTRAINT "REL_c924581ed54ddae034f24955f8" UNIQUE ("addressId"), CONSTRAINT "PK_ee7a215c6cd77a59e2cb3b59d41" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "admins" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_e3b38270c97a854c48d2e80874e" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "positions" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP DEFAULT now(), "updatedAt" TIMESTAMP DEFAULT now(), "target" boolean NOT NULL, "goalkeeper" boolean NOT NULL, "leftwing" boolean NOT NULL, "rightwing" boolean NOT NULL, "fixed" boolean NOT NULL, CONSTRAINT "PK_17e4e62ccd5749b289ae3fae6f3" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "social networks" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "linkedin" character varying, "whatsApp" character varying, "facebook" character varying, "tiktok" character varying, "instagram" character varying, CONSTRAINT "PK_cd24b1a9b28f5599743ec1811ff" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "isActive" boolean NOT NULL DEFAULT true, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "age" integer NOT NULL, "height" integer NOT NULL, "weight" integer NOT NULL, "telephone" character varying NOT NULL, "isExercising" boolean NOT NULL, "urlImg" character varying NOT NULL, "socialNetworkId" uuid, "addressId" uuid, "positionsId" uuid, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "REL_eb49e62a36e9a66901b28504d1" UNIQUE ("socialNetworkId"), CONSTRAINT "REL_bafb08f60d7857f4670c172a6e" UNIQUE ("addressId"), CONSTRAINT "REL_54144931128233282ab3bf7ef1" UNIQUE ("positionsId"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
@@ -55,10 +55,10 @@ export class createTables1667907466810 implements MigrationInterface {
         await queryRunner.query(`DROP TABLE "users"`);
         await queryRunner.query(`DROP TABLE "social networks"`);
         await queryRunner.query(`DROP TABLE "positions"`);
-        await queryRunner.query(`DROP TABLE "admins"`);
         await queryRunner.query(`DROP TABLE "fields"`);
         await queryRunner.query(`DROP TABLE "owners"`);
         await queryRunner.query(`DROP TABLE "addresses"`);
+        await queryRunner.query(`DROP TABLE "admins"`);
     }
 
 }
