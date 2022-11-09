7eb1364b9592cf7a9ee243f97e43bb00094a4f21
lgrimm6 <robertofernandesfreitas@gmail.com>
2022-11-09 18:59:03 -0300

    Feature: admin routes

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,10 @@ import { AppError } from './errors/appError';
 import userLoginRouter from './router/login/userLogin.routes';
 import ownerRouter from './router/owners/owners.routes';
 import ownerLoginRouter from './router/login/ownerLogin.routes';
-import filedsRouter from "./router/fields/fields.routes"
+import filedsRouter from './router/fields/fields.routes';
 import requestsRoutes from './router/requests/requests.routes';
+import adminsRoutes from './router/admins/admins.routes';
+import adminLoginRouter from './router/login/adminLogin.routes';
 
 const app = express();
 app.use(express.json());
@@ -20,8 +22,10 @@ app.use('/participants', participantesRoutes);
 app.use('/users/login', userLoginRouter);
 app.use('/owners', ownerRouter);
 app.use('/owners/login', ownerLoginRouter);
-app.use('/fields', filedsRouter)
-app.use('/requests', requestsRoutes)
+app.use('/admin/login', adminLoginRouter);
+app.use('/fields', filedsRouter);
+app.use('/requests', requestsRoutes);
+app.use('/admin', adminsRoutes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
