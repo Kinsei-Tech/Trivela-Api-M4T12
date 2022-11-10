cc5f7e65d47c03933702f05b5ceea2970240f6a6
richzago <rich.czago@gmail.com>
2022-11-09 21:44:46 -0300

    feat: created the adm delete mocks

---
+++ src/__tests__/mocks/adminFields/index.ts
@@ -0,0 +1,18 @@
+export const mockedAdminDelete = {
+  email: 'richardzago@email.com',
+  name: 'Richard Zago',
+  password: '123456',
+};
+
+export const mockedField2 = {
+  name: 'Quadra Administradores',
+  address: {
+    street: 'Rua Bento',
+    district: 'Rio Grande do Sul',
+    zipCode: '36035-150',
+    number: '48',
+    complement: 'apart. 503',
+    city: 'Porto Alegre',
+    state: 'RS',
+  },
+};
