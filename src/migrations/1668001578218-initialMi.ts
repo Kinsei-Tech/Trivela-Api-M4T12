76dc5b8021bf51a9ab8bdcdac11cbee5070113a8
richzago <rich.czago@gmail.com>
2022-12-13 17:53:12 -0300

    created migrations

---
+++ src/migrations/1668001578218-initialMi.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class initialMi1668001578218 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
